056d25d85a6360f46bf9c6819b1756bd8ca4cf91
p1yu5h0 <piyush200205@gmail.com>
2021-11-02 12:12:28 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
-
+import Sidebar from './Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
         <h1>Let's Build Discord !!</h1>
+        {/*SideBar*/}
+        <Sidebar />
+        
+        {/*Chat*/}
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./sidebar.css";
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>I am the SideBar !</h2>
+
+            <div className="sidebar__top">
+                <h2>Piyush Agrawal</h2>
+                {/* <FontAwesomeIcon icon="fa-solid fa-caret-down" /> */}
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;

fe47d4c86af335e12d6713bbf140cc377c77ad9c
p1yu5h0 <piyush200205@gmail.com>
2021-11-05 17:38:55 +0530

    updated login page

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import "./Login.css";
+import { Button } from 'react-bootstrap'
 
 function Login() {
+
+    const signIn = e => {
+
+    }
+
     return (
         <div className="login">
-            <h2>I am the Login Page</h2>
+            {/* <h2>I am the Login Page</h2> */}
+            <div className="login__logo">
+                <img src="https://www.freepnglogos.com/uploads/discord-logo-png/discord-branding-2.png" alt="discord branding" />
+            </div>
+
+            <Button onClick={signIn} >Sign In</Button>
         </div>
     )
 }
